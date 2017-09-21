64c0bb394b76773974b774c16d0360e717248986
taappana <herranen.jani@gmail.com>
2017-09-21 20:40:52 +0300

    Added react-router and basic routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
+import NotFound from './components/NotFound';
 import StorePicker from './components/StorePicker';
 
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
 
-render(<App/>, document.querySelector('#main'));
+render(<Root/>, document.querySelector('#main'));

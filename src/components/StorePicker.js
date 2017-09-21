88203f793c23b3ad53e5076c722217071a1b4d7f
taappana <herranen.jani@gmail.com>
2017-09-21 21:27:14 +0300

    Now can get data after unsubmit and redirecting to store page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,15 +1,31 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You changed the URL');
+    // First grap the text from the box
+    const storeId = this.storeInput.value;
+    console.log(`Going to ${storeId}`);
+    // Second transition from / to /store/:storeId
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" />
+        <input type="text" required placeholder="Store Name"
+        defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store →</button>
       </form>
     )
   }
+}
+
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;

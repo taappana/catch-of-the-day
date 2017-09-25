129baf0dd7b7184af8ac2e1e08619372afc3099a
taappana <herranen.jani@gmail.com>
2017-09-25 18:53:09 +0300

    Added and synced Firebase to the project

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB1ADQcDprNm6SqDwxZ50fQSOyZS8q8U5A",
+  authDomain: "catch-of-the-day-taappana.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-taappana.firebaseio.com",
+});
+
+export default base;
